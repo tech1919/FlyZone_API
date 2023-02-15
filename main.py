936935755afb4911e9d2c7e8dd39234b714526bc
UriyaTech19 <116645319+UriyaTech19@users.noreply.github.com>
2023-02-15 10:12:25 +0200

    basic api

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+import numpy as np
+from typing import List , Any
+from fastapi import FastAPI
+from fastapi import File, UploadFile , Depends
+
+
+
+# import keras
+
+
+app = FastAPI(
+    title = "FlyZone API",
+)
+
+
+
+
+
